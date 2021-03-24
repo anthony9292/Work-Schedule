7ac537c457e34508b57b7816de9df60e0f8bec57
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-24 16:24:14 -0400

    update

---
+++ assets/script.js
@@ -0,0 +1,84 @@
+$("#todayDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
+
+// this function displays the current date and time
+setInterval(function () {
+  $("#todayDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
+}, 1000);
+
+const createContainer = $(".container");
+const saveButton = document.querySelectorAll("button");
+const timeList = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
+const timeId = ["9,", "10", "11", "12", "13", "14", "15", "16", "17"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+for (let i = 0; i < timeList.length; i++) {
+  let createRow = $("<div class ='row time-block'>").attr("id", timeId[i]);
+  let createTime = $("div class  = 'hour col-1'>");
+
+  let createTextarea = $("<textarea class='col-10'>");
+  createTextarea.attr(id, timeList[i]);
+
+  let createButton = $(
+    "<button type='button' class= 'saveBtn col-1 far fa-save'>"
+  );
+
+  createContainer.append(createRow);
+  createTime.text(timeList[i]);
+  createRow.append(createTime);
+
+  createRow.append(createTextarea);
+
+  createButton.text();
+  createRow.append(createButton);
+}
+
+localStorageFunction();
+
+function localStorageFunction() {
+  for (let index = 0; index < numbers.length; index++) {
+    $("textarea")[index].value = localStorage.getItem(
+      "textarea" + String(index + 1)
+    );
+  }
+}
+
+$("button").on("click", function (event) {
+  event.preventDefault();
+
+  for (let index = 0; index < numbers.length; index++) {
+    localStorage.setItem(
+      "textarea" + String(index + 1),
+      $("textarea")[index].value
+    );
+  }
+});
+
+function updatePerHour() {
+  var currentHour = moment().hours();
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split(" ")[0]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClASS("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClASS("past");
+      $(this).removeClASS("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+updatePerHour();
