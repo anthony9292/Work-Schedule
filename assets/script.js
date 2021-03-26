0b32e0fd5d2f9330469a355f4d562bd712a72d35
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-26 17:54:40 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
+//tells engine to load html & css first 
 $(document).ready(function(){ 
+   //displays the date & time 
 $("#today").text(moment().format('MMMM Do YYYY, h:mm:ss a')); 
 $(".saveBtn").on("click", function () { 
-
+//get the nearby values
 console.log(this); 
 var text = $(this).siblings(".description").val();
 var time = $(this).parent().attr("id"); 
-
+//sets the items into local storage.
 localStorage.setItem(time, text); 
 })
+//laod any save from the saved data from LocalStorage
 $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -21,13 +24,13 @@ $("#hour8 .description").val(localStorage.getItem("hour8"));
 
 
 function onupdate() { 
-
+//gets the current number of hours 
 var currentHour = moment().hour(); 
-
+//loops over time blocks 
 $(".time-block").each(function(){ 
 var blockHour = parseInt($(this).attr("id").split("hour")[1]);
      console.log( blockHour, currentHour)
-
+     //checks if we have moved past the listed time, clicks into html/css disposed classed of past, present, or future 
      if (blockHour < currentHour) { 
       $(this).addClass("past");
       $(this).removeClass("future");
@@ -45,7 +48,7 @@ var blockHour = parseInt($(this).attr("id").split("hour")[1]);
 
 }
 
- onupdate();
+ onupdate(); // re-runs the function 
 })
 
 
